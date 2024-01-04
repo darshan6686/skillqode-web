ea69621b1391f2607d4ce497fb03fbf88a056404
darshan <darshpansheriya2510@gmail.com>
2024-01-04 14:47:21 +0530

    placement pages

---
+++ src/placement/placement.js
@@ -0,0 +1,313 @@
+new Glide(".student3", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4, // Number of visible slides at a time
+    breakpoints: {
+      1355: {
+        perView: 4,
+        gap: 80
+      },
+      1280: {
+        perView: 4,
+      },
+      1100: {
+        perView: 3,
+        gap: -40
+      },
+      1020: {
+        perView: 4
+      },
+      910: {
+        perView: 3
+      },
+      685: {
+        perView: 2,
+        gap: -60
+      },
+      640: {
+        perView: 3
+      },
+      500: {
+        perView: 2,
+        gap: -40,
+      },
+      400: {
+        perView: 2
+      },
+      340: {
+        perView: 1,
+        gap: -110
+      },
+      300: {
+        perView: 1
+      }
+    },
+  
+    gap: 16, // Adjust this value to control the gap between slides
+    autoplay: 200, // Set to 0 to disable autoplay or adjust the delay in milliseconds
+    hoverpause: true, // Pause autoplay on hover
+    focusAt: "center", // Center the active slide
+    bound: true, // Allow continuous looping
+    animationTimingFunc: "linear",
+    animationDuration: 5000,
+  }).mount();
+
+
+new Glide(".placement", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4, // Number of visible slides at a time
+    breakpoints: {
+      1355: {
+        perView: 4,
+        gap: 80
+      },
+      1280: {
+        perView: 4,
+      },
+      1100: {
+        perView: 3,
+        gap: -40
+      },
+      1020: {
+        perView: 4
+      },
+      910: {
+        perView: 3
+      },
+      685: {
+        perView: 2,
+        gap: -60
+      },
+      640: {
+        perView: 3
+      },
+      500: {
+        perView: 2,
+        gap: -40,
+      },
+      400: {
+        perView: 2
+      },
+      340: {
+        perView: 1,
+        gap: -110
+      },
+      300: {
+        perView: 1
+      }
+    },
+  
+    gap: 16, // Adjust this value to control the gap between slides
+    autoplay: 200, // Set to 0 to disable autoplay or adjust the delay in milliseconds
+    hoverpause: true, // Pause autoplay on hover
+    focusAt: "center", // Center the active slide
+    bound: true, // Allow continuous looping
+    direction: "rtl",
+    animationTimingFunc: "linear",
+    animationDuration: 5000,
+  }).mount();
+
+
+  new Glide(".plcament2", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4, // Number of visible slides at a time
+    breakpoints: {
+      1355: {
+        perView: 4,
+        gap: 80
+      },
+      1280: {
+        perView: 4,
+      },
+      1100: {
+        perView: 3,
+        gap: -40
+      },
+      1020: {
+        perView: 4
+      },
+      910: {
+        perView: 3
+      },
+      685: {
+        perView: 2,
+        gap: -60
+      },
+      640: {
+        perView: 3
+      },
+      500: {
+        perView: 2,
+        gap: -40,
+      },
+      400: {
+        perView: 2
+      },
+      340: {
+        perView: 1,
+        gap: -110
+      },
+      300: {
+        perView: 1
+      }
+    },
+  
+    gap: 16, // Adjust this value to control the gap between slides
+    autoplay: 200, // Set to 0 to disable autoplay or adjust the delay in milliseconds
+    hoverpause: true, // Pause autoplay on hover
+    focusAt: "center", // Center the active slide
+    bound: true, // Allow continuous looping
+    animationTimingFunc: "linear",
+    animationDuration: 5000,
+  }).mount();
+
+
+  new Glide(".plcament3", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4, // Number of visible slides at a time
+    breakpoints: {
+      1355: {
+        perView: 4,
+        gap: 80
+      },
+      1280: {
+        perView: 4,
+      },
+      1100: {
+        perView: 3,
+        gap: -40
+      },
+      1020: {
+        perView: 4
+      },
+      910: {
+        perView: 3
+      },
+      685: {
+        perView: 2,
+        gap: -60
+      },
+      640: {
+        perView: 3
+      },
+      500: {
+        perView: 2,
+        gap: -40,
+      },
+      400: {
+        perView: 2
+      },
+      340: {
+        perView: 1,
+        gap: -110
+      },
+      300: {
+        perView: 1
+      }
+    },
+  
+    gap: 16, // Adjust this value to control the gap between slides
+    autoplay: 200, // Set to 0 to disable autoplay or adjust the delay in milliseconds
+    hoverpause: true, // Pause autoplay on hover
+    focusAt: "center", // Center the active slide
+    bound: true, // Allow continuous looping
+    direction: "rtl",
+    animationTimingFunc: "linear",
+    animationDuration: 5000,
+  }).mount();
+
+
+
+  new Glide(".plcament4", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4, // Number of visible slides at a time
+    breakpoints: {
+      1355: {
+        perView: 4,
+        gap: 80
+      },
+      1280: {
+        perView: 4,
+      },
+      1100: {
+        perView: 3,
+        gap: -40
+      },
+      1020: {
+        perView: 4
+      },
+      910: {
+        perView: 3
+      },
+      685: {
+        perView: 2,
+        gap: -60
+      },
+      640: {
+        perView: 3
+      },
+      500: {
+        perView: 2,
+        gap: -40,
+      },
+      400: {
+        perView: 2
+      },
+      340: {
+        perView: 1,
+        gap: -110
+      },
+      300: {
+        perView: 1
+      }
+    },
+  
+    gap: 16, // Adjust this value to control the gap between slides
+    autoplay: 200, // Set to 0 to disable autoplay or adjust the delay in milliseconds
+    hoverpause: true, // Pause autoplay on hover
+    focusAt: "center", // Center the active slide
+    bound: true, // Allow continuous looping
+    animationTimingFunc: "linear",
+    animationDuration: 5000,
+  }).mount();
+
+
+
+  // -----------------------------------------------------------------
+//               SECTION 3 ==> Our Hiring Partner
+// -----------------------------------------------------------------
+
+$("#slick1").slick({
+    rows: 2,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow:
+      '<button class="slide-arrow prev-arrow mt-10 bg-[#ed2224] hover:text-[#ed2224] hover:bg-white hover:duration-200 text-white w-7 sm:w-10 h-7 sm:h-10 rounded-full"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow:
+      '<button class="slide-arrow next-arrow mt-10 bg-[#ed2224] hover:text-[#ed2224] hover:bg-white hover:duration-200 text-white w-7 sm:w-10 h-7 sm:h-10 rounded-full"><i class="fa-solid fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
